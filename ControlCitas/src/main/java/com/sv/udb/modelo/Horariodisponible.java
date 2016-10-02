5dfcc9257735f261acad2d3ca54e7e7312a57f71
Kevin <Kevin@192.168.1.6>
2016-10-02 16:19:40 -0600

    asignado target="body" a todos los ficheros javascript, para evitar conflictos con algunos componentes de PrimeFaces, y evitar errores de javascript

--- ControlCitas/src/main/java/com/sv/udb/modelo/Horariodisponible.java
+++ ControlCitas/src/main/java/com/sv/udb/modelo/Horariodisponible.java
@@ -6,23 +6,18 @@
 package com.sv.udb.modelo;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
@@ -59,7 +54,7 @@ public class Horariodisponible implements Serializable {
     private String diaHoraDisp;
     @Basic(optional = false)
     @NotNull
-    @Size(min = 1, max = 5)
+    @Size(min = 1, max = 8)
     @Column(name = "hora_inic_hora_disp")
     private String horaInicHoraDisp;
     @Basic(optional = false)
@@ -76,8 +71,6 @@ public class Horariodisponible implements Serializable {
     @NotNull
     @Column(name = "esta_hora_disp")
     private int estaHoraDisp;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "codiHoraDisp", fetch = FetchType.EAGER)
-    private List<Excepcionhorariodisponible> excepcionhorariodisponibleList;
 
     public Horariodisponible() {
     }
@@ -150,15 +143,6 @@ public class Horariodisponible implements Serializable {
 
     public void setEstaHoraDisp(int estaHoraDisp) {
         this.estaHoraDisp = estaHoraDisp;
-    }
-
-    @XmlTransient
-    public List<Excepcionhorariodisponible> getExcepcionhorariodisponibleList() {
-        return excepcionhorariodisponibleList;
-    }
-
-    public void setExcepcionhorariodisponibleList(List<Excepcionhorariodisponible> excepcionhorariodisponibleList) {
-        this.excepcionhorariodisponibleList = excepcionhorariodisponibleList;
     }
 
     @Override
